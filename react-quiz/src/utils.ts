d14510f5d77b3b92bd9353640691c0267e5525fb
JB <ampzgunz@hotmail.com>
2021-10-17 22:48:10 -0400

    Delete react-quiz directory

--- react-quiz/src/utils.ts
+++
@@ -1,4 +0,0 @@
-// This will handle shuffling of arrays within our quiz
-export const shuffleArray = (array: any[]) => 
-// Creating a new array via spread and then sorting it and applying random result
-[...array].sort(() => Math.random() - 0.5);
